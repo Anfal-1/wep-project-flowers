6653b0950c10a7f72aa22e315d457172bd77d845
hanadyal7arby <165439701+hanadyal7arby@users.noreply.github.com>
2024-04-17 21:41:50 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
-document.querySelector(".bars-menu").addEventListener("click", () => {
-  document.querySelector(".nav-items").classList.toggle("active");
-});
-
-document.querySelectorAll(".nav-link").forEach((item) => {
-  item.addEventListener("click", () => {
-    document.querySelector(".nav-items").classList.toggle("active");
-  });
-});
-
+function product(){
+  var Flower1=parseInt(document.getElementById("flower1").value);
+  var Flower2=parseInt(document.getElementById("flower2").value);
+  var Flower3=parseInt(document.getElementById("flower3").value);
+  var Flower4=parseInt(document.getElementById("flower4").value);
+  var result=0;
+  var act= document.getElementById("SelectActive").value;
+  if(act==="Red Rose"){
+    result=150;
+  }
+  else if(act==="White Tulips"){
+    result=190;
+  }
+  else if(act==="Rosy Love"){
+    result=200;
+  }
+  else if (act==="Red Baby Roses"){
+    result=250;
+  }
+    alert(document.getElementById("totalproduct").innerHTML=Math.round(result));
+}

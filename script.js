59c74b076f5521e530dd50886ad1faa3b838b3eb
Anfal-1 <135338207+Anfal-1@users.noreply.github.com>
2024-04-18 03:20:33 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const addToCartButtons = document.querySelectorAll(".add-to-cart-button");
-    addToCartButtons.forEach(button => {
-        button.addEventListener("click", function(event) {
-            event.preventDefault();
-            const product = this.parentNode.querySelector('input[name="product"]').value;
-            alert(Added ${product} to cart!); // Replace this with your actual add to cart logic
-        });
-    });
-});
+

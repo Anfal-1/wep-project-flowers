0f4b91a40f06f3f223a7de39677433c25711f7c6
LanaSultaan <165483374+LanaSultaan@users.noreply.github.com>
2024-04-18 05:22:28 +0300

    ايقونة السلة chdisplay.php

--- chdisplay.php
+++ chdisplay.php
@@ -49,8 +49,9 @@ if (isset($_SERVER['REQUEST_METHOD']) && $_SERVER['REQUEST_METHOD'] == 'POST') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>LARH - Checkout</title>
-        <link rel="stylesheet" href="styles.css"> 
+    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <title>LARH - Checkout</title> 
 </head>
 <body>
     <header>
@@ -60,9 +61,9 @@ if (isset($_SERVER['REQUEST_METHOD']) && $_SERVER['REQUEST_METHOD'] == 'POST') {
                 <li><a href="index.html">Home</a></li>
                 <li><a href="about.html">About Us</a></li>
                 <li><a href="add.php">Products</a></li>
-                <li><a href="cart.php">cart</a></li>
                 <li><a href="chdisplay.php">Checkout</a></li>
                 <li><a href="contact.html">Contact Us</a></li>
+                <li><a href="cart.php"><i class="fas fa-shopping-cart nav-cart-icon"></i></a></li>
             </ul>
         </nav>
     </header>

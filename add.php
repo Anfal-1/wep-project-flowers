b2069aaded8aa36c977d74e7bb2274a6907bccc3
Anfal-1 <135338207+Anfal-1@users.noreply.github.com>
2024-04-19 10:53:16 +0300

    Update add.php d

--- add.php
+++ add.php
@@ -6,12 +6,14 @@ $database = new PDO("mysql:host=localhost;dbname=web;charset=utf8", $username, $
 if (isset($_POST['product'], $_POST['price'])) {
     $product = $_POST['product'];
     $price = $_POST['price'];
+    $quantity = $_POST['quantity'];
     // Use placeholders for values to prevent SQL injection
-    $addData = $database->prepare("INSERT INTO cart (product ,price) VALUES (:product ,:price)");
+    $addData = $database->prepare("INSERT INTO cart (product ,price ,quantity) VALUES (:product ,:price ,:quantity)");
     
     // Bind the parameters to the actual values
     $addData->bindParam(':product', $product);
     $addData->bindParam(':price', $price);
+    $addData->bindParam(':quantity', $quantity);
     if ($addData->execute()) {
         echo 'تم بنجاح اضافة بيانات';
     } else {
@@ -47,7 +49,7 @@ if (isset($_POST['product'], $_POST['price'])) {
                 <li><a href="index.html">Home</a></li>
                 <li><a href="about.html">About Us</a></li>
                 <li><a href="add.php">Products</a></li>
-                <li><a href="chdisplay.php">Checkout</a></li>
+               
                 <li><a href="contact.html">Contact Us</a></li>
                 <li><a href="cart.php"><i class="fas fa-shopping-cart nav-cart-icon"></i></a></li>
             </ul>
@@ -61,8 +63,10 @@ if (isset($_POST['product'], $_POST['price'])) {
             <h3>Red Roses</h3>
             <p>Price: SAR 150</p>
             <form method="post">
-            <input type="hidden" name="product" value="Red Roses">
+            <input type="hidden" name="product" id="flower" value="Red Roses">
             <input type="hidden" name="price" value="150">
+            <label for="quantity">Quantity:</label>
+        <input type="number" id="quantity" name="quantity" min="1" max="100" value="1">
             <button class="add-to-cart-button" data-product-name="Red Roses" data-product-price="150">Add to Cart</button>
 
             </form>
@@ -73,8 +77,10 @@ if (isset($_POST['product'], $_POST['price'])) {
             <h3>White Tulips</h3>
             <p>Price: SAR 190</p>
             <form method="post">
-            <input type="hidden" name="product" value="White Tulips">
-            <input type="hidden" name="price" value="190">
+            <input type="hidden" name="product"  id="flower"  value="White Tulips">
+            <input type="hidden" id="price" name="price" value="190">
+            <label for="quantity">Quantity:</label>
+        <input type="number" id="quantity" name="quantity" min="1" max="100" value="1">
             <button class="add-to-cart-button" data-product-name="White Tulips" data-product-price="190">Add to Cart</button>
 
             </form>
@@ -84,8 +90,10 @@ if (isset($_POST['product'], $_POST['price'])) {
             <h3>Rosy Love</h3>
             <p>Price: SAR 200</p>
             <form method="post">
-            <input type="hidden" name="product" value="Rosy Love">
+            <input type="hidden" name="product"  id="flower"  value="Rosy Love">
             <input type="hidden" name="price" value="200">
+            <label for="quantity">Quantity:</label>
+        <input type="number" id="quantity" name="quantity" min="1" max="100" value="1">
             <button class="add-to-cart-button" data-product-name="Rosy Love" data-product-price="200">Add to Cart</button>
 
             </form>
@@ -95,8 +103,10 @@ if (isset($_POST['product'], $_POST['price'])) {
             <h3>Red Baby Roses</h3>
             <p>Price: SAR 250</p>
             <form method="post">
-            <input type="hidden" name="product" value="Red Baby Roses">
+            <input type="hidden" name="product"  id="flower"  value="Red Baby Roses">
             <input type="hidden" name="price" value="250">
+            <label for="quantity">Quantity:</label>
+        <input type="number" id="quantity" name="quantity" min="1" max="100" value="1">
             <button class="add-to-cart-button" data-product-name="Red Baby Roses" data-product-price="250">Add to Cart</button>
             </form>
         </div>
